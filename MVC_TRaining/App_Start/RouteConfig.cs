54444c6611fada4d33291f775b339ea81fe643a9
nvn01 <nvn01@LAPTOP-1JAUQP44>
2019-12-21 08:00:55 +0530

    Comminiting just default

--- MVC_TRaining/App_Start/RouteConfig.cs
+++ MVC_TRaining/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC_TRaining
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Default", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MVC_TRaining/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TRaining.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public string Default()
+        {
+            return "Pass the controller/index name in URL?";
+        }
+        public string Index()
+        {
+            ViewBag.passSomeData = "Hello data came fromm controller to view ";
+            //returning just string 
+            return "Hi There!!!!!! Returning just string :)";
+            //return View();
+        }
+    }
+}
